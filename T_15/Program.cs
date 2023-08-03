3a2ac018bb5036190d26be69fb2a6732819868a9
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-08-03 12:24:13 +0500

    Initial commit

---
+++ T_15/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 1;
+        }
+    }
+}
