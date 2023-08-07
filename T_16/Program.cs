5ddf80ea7701dfe0c8e8f89e12b151576339f824
MaksimMelan <64986385+MaksimMelan@users.noreply.github.com>
2023-08-07 20:17:20 +0500

    Катрное N - 02

--- T_16/Program.cs
+++ T_16/Program.cs
@@ -4,35 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace T_16
+namespace CSHomeworkCycles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int count = 0;
-            int numMin = 100;
-            int numMax = 999;
-            
-
+            int startInterval = 1;
+            int endInterval = 27;
+            int maxNumberToCheck = 1000;
+            int minNumberToCheck = 100;
+            int numberN;
             Random random = new Random();
-            int n = random.Next(1, 27);
+            int counter = 0;
 
-            while(numMin <= numMax)
+            numberN = random.Next(startInterval, endInterval);
+
+            for (int i = 0; i < maxNumberToCheck; i += numberN)
             {
-                numMin++;
-                while (numMin >= n)
+                if (i >= minNumberToCheck)
                 {
-                    numMin -= n;
-                    
-                    if (numMin == 0);
-                    {
-                        count++;
-                    }
+                    Console.WriteLine($"картное число {i}\n");
+                    counter++;
                 }
-                
             }
-            Console.WriteLine($"количество трехзначных натуральных чисел равно - {count}. Число N равно - {n}");
+            Console.WriteLine($"Число N - {numberN}");
+            Console.WriteLine($"Трехзначных чисел кратных N - {counter}");
         }
     }
 }
