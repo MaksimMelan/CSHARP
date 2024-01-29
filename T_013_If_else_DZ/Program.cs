b0c0ca5c68a1068d0f81fa1ba1cf054e31611ad3
maksimovmaks <maksimum884@gmail.com>
2024-01-29 19:26:28 +0500

    T_013_If_else_DZ
    чётное не четное число

--- T_013_If_else_DZ/Program.cs
+++ T_013_If_else_DZ/Program.cs
@@ -11,14 +11,14 @@ namespace T_013_If_else_DZ
         static void Main(string[] args)
         {
             double number;
-            double remains;
+            
 
             Console.WriteLine("Введите число:");
             number = Convert.ToDouble(Console.ReadLine());
 
-            remains = number % 2;
+            
 
-            if ( remains == 0)
+            if ( 0 == number % 2 )
             {
                 Console.WriteLine("Число " + number + " чётное!");
             }
