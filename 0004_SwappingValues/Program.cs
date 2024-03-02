04f3967f7a262a8f29a072dfc7ce5f3b766e4863
maksimovmaks <maksimum884@gmail.com>
2024-03-02 12:51:32 +0500

    0004_SwappingValues

---
+++ 0004_SwappingValues/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _0004_SwappingValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Полное имя: ";
+            string name= "Иванов ";
+            string surname = "Иван ";
+            string clipboardName;
+
+            Console.WriteLine(fullName + name + surname);
+
+            clipboardName = name;
+            name = surname;
+            surname = clipboardName;
+
+            Console.WriteLine();
+            Console.WriteLine(fullName + name + surname);
+
+            Console.ReadKey();
+        }
+    }
+}
