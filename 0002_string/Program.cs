9d34ec2d4bd2429f603cfe108e2bb17738e31e9e
maksimovmaks <maksimum884@gmail.com>
2024-03-02 10:58:55 +0500

    0002_string

---
+++ 0002_string/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0002_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string age;
+            string cityResidence;
+            string profession;
+
+            Console.WriteLine("Как Вас зовут?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет?");
+            age = Console.ReadLine();
+
+            Console.WriteLine("Место проживания? ");
+            cityResidence = Console.ReadLine();
+
+            Console.WriteLine("Кем вы работаете?");
+            profession = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"Вас зовут {name}. Вам {age} лет. Ваше место проживания {cityResidence} . Вы работате {profession}. ");
+
+            Console.ReadKey();
+        }   
+    }
+}
