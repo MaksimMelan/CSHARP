68689eb5b71e78acba1996d9e338b74217eaaee1
maksimovmaks <maksimum884@gmail.com>
2024-03-02 11:49:44 +0500

    0003_Pictures

---
+++ 0003_Pictures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _0003_Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countImages = 52;
+            int countRows = 3;
+            int countString;
+            float countSurplus;
+
+            countString = countImages / countRows;
+            countSurplus = Convert.ToSingle(countImages) % countRows;
+
+            Console.WriteLine($"Полностью заполненных раядов {countString}. Лишних картин {countSurplus}");
+
+            Console.ReadKey();
+        }
+    }
+}
