21cb6adc7f00fa362eb6bb7c37256745dd96c10a
maksimovmaks <maksimum884@gmail.com>
2024-03-05 19:17:28 +0500

    0007_MasteringСycles

---
+++ 0007_MasteringСycles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _0007_MasteringСycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageRequestInput = "Напишите фразу которую нужно повторить. ";
+            string messageRequestCountRepeats = "Напишите количество повторов, цифрой";
+            string inputMessag;
+
+            int countRepeats;
+
+            Console.WriteLine(messageRequestInput);
+            inputMessag = Console.ReadLine();
+
+            Console.WriteLine(messageRequestCountRepeats);
+            countRepeats = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < countRepeats; i++)
+            {
+                Console.WriteLine(inputMessag);
+            }
+            Console.ReadKey();
+        }
+    }
+}
