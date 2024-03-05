0af4b204d317c8d540998f77ff4323ace790e7ba
maksimovmaks <maksimum884@gmail.com>
2024-03-05 13:46:13 +0500

    timeOnePersonInMinutes

--- 0006_Polyclinic/Program.cs
+++ 0006_Polyclinic/Program.cs
@@ -6,13 +6,13 @@ namespace _0006_Polyclinic
     {
         static void Main(string[] args)
         {
-            int MinutesInHour = 60;
+            const int MinutesInHour = 60;
 
             string messageInputCountPeople = "Введите колличестов людей в очереди: ";
             string messageWaitingTime = "Время ожидания вашей очереди - ";
 
             int countPeople = 0;
-            int timeOnePerson = 10;
+            int timeOnePersonInMinutes = 10;
             int waitingTimeMinutes = 0;
             int waitingTimeHours = 0;
             int waitingTimeMinutesStat = 0; 
@@ -21,10 +21,9 @@ namespace _0006_Polyclinic
 
             countPeople = int.Parse(Console.ReadLine());
 
-            waitingTimeMinutes = countPeople * timeOnePerson;
+            waitingTimeMinutes = countPeople * timeOnePersonInMinutes;
             waitingTimeHours = waitingTimeMinutes / MinutesInHour;
             waitingTimeMinutesStat = waitingTimeMinutes % MinutesInHour;
-
 
             Console.WriteLine(messageWaitingTime + waitingTimeHours + " часа " + waitingTimeMinutesStat + " минут.");
 
