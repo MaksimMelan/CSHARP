c206a759ec2cd82e327f810874407dc494a39b3c
maksimovmaks <maksimum884@gmail.com>
2024-03-06 12:50:44 +0500

    exit

---
+++ 0008_OutputСontrol/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _0008_OutputСontrol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+
+            Console.WriteLine($"Введите слово 'exit'  для выхода из программы.");
+            Console.WriteLine();
+
+            while (input != "exit")
+            {
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Выход из программы.");
+        }
+    }
+}
