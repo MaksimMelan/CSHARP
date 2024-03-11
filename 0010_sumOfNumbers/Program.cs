e9e2137a347eaa2f6b730a5cb14a3bf25924a73b
maksimovmaks <maksimum884@gmail.com>
2024-03-11 10:55:02 +0500

    sumOfNumbers

---
+++ 0010_sumOfNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _0010_sumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int randomNumber = rnd.Next(1, 100);
+
+            while (randomNumber >= 0)
+            {
+                Console.WriteLine(randomNumber);
+
+                randomNumber += randomNumber--;
+
+                if (randomNumber <= 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine(randomNumber);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
