920bd75089d7f2e110f268285ca161a486bf74db
maksimovmaks <maksimum884@gmail.com>
2024-03-11 09:06:49 +0500

    Sequence

---
+++ 0009_Sequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _0009_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minCount = 5;
+            int maxCount = 103;
+            int progressionDifference = 7;
+
+            for (int i = minCount; i <= maxCount; i += progressionDifference)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
