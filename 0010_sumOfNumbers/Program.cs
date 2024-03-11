89db3bbcdc0dbc4a467b2533a34e697be2325403
maksimovmaks <maksimum884@gmail.com>
2024-03-11 15:23:39 +0500

    sumOfNumbers_002

--- 0010_sumOfNumbers/Program.cs
+++ 0010_sumOfNumbers/Program.cs
@@ -8,22 +8,23 @@ namespace _0010_sumOfNumbers
         {
             Random rnd = new Random();
 
-            int randomNumber = rnd.Next(1, 100);
+            int randomNumber = rnd.Next(1, 101);
 
-            while (randomNumber >= 0)
+            int sum = 0;
+            int multiplicityOfNumberOne = 3;
+            int multiplicityOfNumberTwo = 5;  
+
+            for (int i = 0; i <= randomNumber; i++)
             {
-                Console.WriteLine(randomNumber);
-
-                randomNumber += randomNumber--;
-
-                if (randomNumber <= 0)
+              
+                if (i % multiplicityOfNumberOne == 0 || i % multiplicityOfNumberTwo == 0)
                 {
-                    break;
+                    sum += i;                       
                 }
-
-                Console.WriteLine(randomNumber);
             }
 
+            Console.WriteLine($"Сгенерированно число {randomNumber}");
+            Console.WriteLine($"Сумма всех положительных чисел, меньших или равных {randomNumber}, кратных {multiplicityOfNumberOne} или {multiplicityOfNumberTwo}: {sum}");
             Console.ReadKey();
         }
     }
