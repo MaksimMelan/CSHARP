83965b698c2b86b6cdaccf4bba1da73894865667
maksimovmaks <maksimum884@gmail.com>
2024-03-12 12:58:06 +0500

    password

---
+++ 0014_password/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _0014_password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "54321";
+            string userInput;
+
+            for (int i = tryCount; i > 0 ; i--)
+            {               
+                Console.Write($"У Вас {i} попытки\n");
+                Console.Write($"Введите пароль: "); ;
+                userInput = Console.ReadLine();
+                if (userInput == password)
+                {
+                    Console.WriteLine("Секретное слово: ПАРОЛЬ.");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            
+            Console.WriteLine("У Вас кончились попытки! Пока! ");
+            Console.ReadKey();
+        }
+    }
+}
