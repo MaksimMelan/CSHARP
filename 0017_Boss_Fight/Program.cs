8fa21ad1443865691d4e23ddcd5d4d8836806cdb
maksimovmaks <maksimum884@gmail.com>
2024-03-12 17:58:57 +0500

    Boss_Fight

---
+++ 0017_Boss_Fight/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0017_Boss_Fight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CommandSimplyAttack = "1";
+            const string CommandFireball = "2";
+            const string CommandExplosion = "3";
+            const string CommandHealing = "4";
+
+            Random random = new Random();
+
+            int healthPointsHero = 1000;
+            int healthPointsBoss = 1000;
+            int defaultHealthPointsHero = 1000;
+
+            int energyHero = 1000;
+            int energyDefaultHero = 1000;
+            int energyConsumptionByFireball = 100;
+
+            int damageSimplyAttack = 50;
+            int damageFireballAttack = 100;
+            int damageExplosionAttack = 200;
+            int healingAttack = 200;
+            int countHealingAttack = 5;
+            int damageAttackBoss = 0;
+
+            bool isUsedFireball = false;
+
+            string inputСommand = "";
+
+            string bossName = "Босс ";
+            string heroName = "Герой ";
+            string messageHealth = " здоровье - ";
+            string messageEnergyHero = "; энергия - ";
+            string playerMenu = "Какие Ваши действия? \nварианты: ";
+            string attackMessage = " атаковал ";
+            string messageUsedFireball = "Фаербол использован! ";
+            string messageNotUsedFireball = "Фаербол не использован";
+            string messageNoEnergyUsedFireball = "Нет энергии! ";
+            string messageInputIrror = "Такой команды не существует!";
+            string messageDeathHero = "Герой умер. Вы проиграли!";
+            string messageDeathBoss = "Босс умер. ВЫ выиграли!";
+            string messageDraw = "Ничья";
+            string messageSimply = " - обычная атака: урон - ";
+            string messageFireball = " - фаербол: урон - ";
+            string messageEnergyConsumptionByFireball = " затраты энергии - ";
+            string messageExplosion = " - взрыв: урон - ";
+            string messageHealing = " - лечение: востонавливает здоровья - ";
+            string messageCountHealingAttack = " осталось лечений - ";
+            string messageSkippingMove = "Пропуск хода!";
+            string messagePlayer = "Готов к бою ";
+            string messageBoss = "Готов к бою  ";
+            string messageNotHealingAttack = "Лечение невозможно";
+            string messageСonditionFireball = " Можно вызывать, только если был использован огненный шар.\n    Для повторного применения надо повторно использовать огненный шар. ";
+            string messageСonditionFireballUsed = "Фаербол не использован ";
+
+            while (healthPointsHero > 0 && healthPointsBoss > 0)
+            {
+                Console.WriteLine(playerMenu +
+                    "\n" + CommandSimplyAttack + messageSimply + damageSimplyAttack +
+                    "\n" + CommandFireball + messageFireball + damageFireballAttack + messageEnergyConsumptionByFireball + energyConsumptionByFireball +
+                    "\n" + CommandExplosion + messageExplosion + damageExplosionAttack + messageСonditionFireball + messageСonditionFireballUsed +
+                    "\n" + CommandHealing + messageHealing + healingAttack + messageCountHealingAttack + countHealingAttack);
+                Console.WriteLine();
+                Console.WriteLine(heroName + messageHealth + healthPointsHero + messageEnergyHero + energyHero);
+                Console.WriteLine(bossName + messageHealth + healthPointsBoss);
+                Console.WriteLine();
+                Console.WriteLine(messagePlayer);
+                Console.WriteLine(messageBoss);
+                Console.Write("Введите номер атаки - ");
+
+                inputСommand = Console.ReadLine();
+
+                Console.WriteLine();
+
+                switch (inputСommand)
+                {
+                    case CommandSimplyAttack:
+                        healthPointsBoss -= damageSimplyAttack;
+                        messagePlayer = heroName + attackMessage + CommandSimplyAttack + messageSimply + damageSimplyAttack;
+                        break;
+
+                    case CommandFireball:
+
+                        if (energyHero >= energyConsumptionByFireball)
+                        {
+                            energyHero -= energyConsumptionByFireball;
+                            healthPointsBoss -= damageFireballAttack;
+                            messagePlayer = heroName + attackMessage + CommandFireball + messageFireball + damageFireballAttack + messageEnergyHero + energyConsumptionByFireball;
+                            isUsedFireball = true;
+                            messageСonditionFireballUsed = messageUsedFireball;
+                            break;
+                        }
+                        else
+                        {
+                            messagePlayer = heroName + messageSkippingMove + messageNoEnergyUsedFireball;
+                        }
+                        break;
+
+                    case CommandExplosion:
+
+                        if (isUsedFireball == true)
+                        {
+                            healthPointsBoss -= damageExplosionAttack;
+                            messagePlayer = heroName + attackMessage + CommandExplosion + messageExplosion + damageExplosionAttack;
+                            isUsedFireball = false;
+                            messageСonditionFireballUsed = messageNotUsedFireball;
+                        }
+                        else
+                        {
+                            messagePlayer = heroName + messageSkippingMove + messageUsedFireball;
+                        }
+                        break;
+
+                    case CommandHealing:
+
+                        if (countHealingAttack > 0)
+                        {
+                            messagePlayer = heroName + attackMessage + CommandHealing + messageHealing + healingAttack;
+                            energyHero = energyDefaultHero;
+                            healthPointsHero += healingAttack;
+
+                            if (healthPointsHero > 1000)
+                            {
+                                healthPointsHero = defaultHealthPointsHero;
+                            }
+                            countHealingAttack--;
+                        }
+                        else
+                        {
+                            messagePlayer = messageNotHealingAttack + messageCountHealingAttack + countHealingAttack;
+                        }
+                        break;
+
+                    default:
+                        messagePlayer = messageInputIrror + messageSkippingMove;
+                        break;
+                }
+
+                damageAttackBoss = (int)random.Next(50, 150);
+                healthPointsHero -= damageAttackBoss;
+                messageBoss = bossName + attackMessage + damageAttackBoss;
+                Console.Clear();
+            }
+
+            if (healthPointsHero > 0 && healthPointsBoss > 0)
+            {
+                Console.WriteLine(messageDraw);
+            }
+            else if (healthPointsBoss > 0)
+            {
+                Console.WriteLine(messageDeathHero);
+            }
+            else
+            {
+                Console.WriteLine(messageDeathBoss);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 0017_Boss_Fight/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("0017_Boss_Fight")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("0017_Boss_Fight")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3d2c915b-c13e-49af-adec-076968db4243")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
