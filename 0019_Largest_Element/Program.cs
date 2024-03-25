827d440e00a4f46a8d961a181e4507e8a71a0f55
maksimovmaks <maksimum884@gmail.com>
2024-03-25 19:26:21 +0500

    0019_Largest_Element

---
+++ 0019_Largest_Element/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _0019_Largest_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberOfRows = 10;
+            int numberOfColumns = 10;
+
+            int minNumberRandomness = 1;
+            int maxNumberRandomness = 100;
+
+            int[,] array = new int[numberOfRows, numberOfColumns];
+
+            int maxElement = int.MinValue;
+
+            Console.WriteLine($"Создан массив");
+            Console.WriteLine();
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    array[i, j] = random.Next(minNumberRandomness, maxNumberRandomness);
+                    Console.Write(array[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    if (array[i, j] > maxElement)
+                    {
+                        maxElement = array[i, j];
+                    }                   
+                }               
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    if (array[i, j] == maxElement)
+                    {
+                         array[i, j] = 0;
+                    }                    
+                }               
+            }
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {                   
+                    Console.Write(array[i, j] + "  ");                  
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Наибольший элемент - {maxElement}");
+            Console.ReadKey();
+        }
+    }
+}
