f86c0f0c32de8af59065f8d5514582383fa66af3
maksimovmaks <maksimum884@gmail.com>
2024-03-28 20:20:59 +0500

    0020_Local_Maxima_01

---
+++ 0020_Local_Maxima/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _0020_Local_Maxima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberOfColumns = 20;
+
+            int minNumberRandomness = -100;
+            int maxNumberRandomness = 100;
+
+            int[] array = new int[numberOfColumns];
+
+            int indentationForDataOutput = 10;
+            int indexNumber = 0;
+            int localMaxima = 0;
+            string messageLocalMaxima = "\nВывод номера индекса и черз дробь(/) его локального максимума: ";
+            Console.WriteLine($"Создан массив");
+            Console.WriteLine();
+
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                array[i] = random.Next(minNumberRandomness, maxNumberRandomness);
+                Console.Write(array[i] + "  ");
+            }
+            Console.WriteLine();
+
+            Console.Write($"{messageLocalMaxima}\n\n");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if ((i == 0 || array[i] > array[i - 1]) && (i == array.Length - 1 || array[i] > array[i + 1]))
+                {
+                    indexNumber = i - 1;
+                    localMaxima = array[i];
+                    Console.WriteLine($"{indexNumber}/{localMaxima}".PadLeft(indentationForDataOutput));
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
