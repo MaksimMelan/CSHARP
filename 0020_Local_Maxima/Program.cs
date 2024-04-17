82b9e01a2c945a7de762999efb08b0f0db5a0e39
maksimovmaks <maksimum884@gmail.com>
2024-04-18 02:40:00 +0500

    Local_Maxima_03

--- 0020_Local_Maxima/Program.cs
+++ 0020_Local_Maxima/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace _0020_Local_Maxima
 {
@@ -16,7 +18,7 @@ namespace _0020_Local_Maxima
             int[] array = new int[numberOfColumns];
 
             int indentationForDataOutput = 10;
-            int indexNumber = 0;
+
             int localMaxima = 0;
 
             string messageLocalMaxima = "\nВывод номера индекса и черз дробь(/) его локального максимума: ";
@@ -35,27 +37,26 @@ namespace _0020_Local_Maxima
 
             if (array[0] > array[1])
             {
-                indexNumber = 0;
                 localMaxima = array[0];
-                Console.WriteLine($"{indexNumber + 1}/{localMaxima}".PadLeft(indentationForDataOutput));
+                Console.WriteLine($"1/{localMaxima}".PadLeft(indentationForDataOutput));
             }
 
             for (int i = 1; i < array.Length - 1; i++)
             {
                 if (array[i] > array[i - 1] && array[i] > array[i + 1])
                 {
-                    indexNumber = i;
                     localMaxima = array[i];
 
-                    Console.WriteLine($"{indexNumber + 1}/{localMaxima}".PadLeft(indentationForDataOutput));
+                    Console.WriteLine($"{i + 1}/{localMaxima}".PadLeft(indentationForDataOutput));
                 }
             }
 
-            if(array[array.Length - 1] > array[array.Length -2])
+            int indexOfLastElement;
+
+            if (array[array.Length - 1] > array[array.Length -2])
             {
-                indexNumber = array.Length;
-                localMaxima = array[array.Length - 1];
-                Console.WriteLine($"{indexNumber}/{localMaxima}".PadLeft(indentationForDataOutput));
+                indexOfLastElement = array[array.Length - 1];
+                Console.WriteLine($"{array.Length}/{indexOfLastElement}".PadLeft(indentationForDataOutput));
             }
 
             Console.WriteLine();
