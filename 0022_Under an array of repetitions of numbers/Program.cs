35da515b4edc8a4f51fdc06c289604f26c24b586
maksimovmaks <maksimum884@gmail.com>
2024-04-27 17:29:10 +0500

    Under_an_array_of_repetitions_of_numbers_01

---
+++ 0022_Under an array of repetitions of numbers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0022_Under_an_array_of_repetitions_of_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberOfColumns = 30;
+
+            int minNumberRandomness = 1;
+            int maxNumberRandomness = 10;
+
+            int[] arrayNumbers = new int[numberOfColumns];
+
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                arrayNumbers[i] = random.Next(minNumberRandomness, maxNumberRandomness);
+
+                Console.WriteLine($"№{i + 1} {arrayNumbers[i]}  ");
+            }
+
+            int currentNum = arrayNumbers[0];
+            int repeatingNumber = 1;
+            int maxNum = arrayNumbers[0];
+            int amountOfRepetitions = 1;
+
+
+            for (int i = 1; i < arrayNumbers.Length; i++)
+            {
+                if (arrayNumbers[i] == currentNum)
+                {
+                    repeatingNumber++;
+                }
+                else
+                {
+                    if (repeatingNumber > amountOfRepetitions)
+                    {
+                        amountOfRepetitions = repeatingNumber;
+                        maxNum = currentNum;
+                    }
+                    currentNum = arrayNumbers[i];
+                    repeatingNumber = 1;
+                }
+            }
+
+            Console.WriteLine("Массив: " + string.Join(", ", arrayNumbers));
+            Console.WriteLine("Число: " + maxNum);
+            Console.WriteLine("Количество повторений: " + amountOfRepetitions);
+            Console.ReadKey();
+        }
+    }
+}
