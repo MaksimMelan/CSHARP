b47a1b8be7ac532cbb73582f9e1f9e0398bffe31
maksimovmaks <maksimum884@gmail.com>
2024-06-05 16:03:53 +0500

    0023_Sorting_numbers_002

--- 0023_Sorting numbers/Program.cs
+++ 0023_Sorting numbers/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0023_Sorting_numbers
 {
@@ -14,7 +10,7 @@ namespace _0023_Sorting_numbers
 
             int numberOfColumns = 10;
 
-            int minNumberRandomness = 1;
+            int minNumberRandomness = -10;
             int maxNumberRandomness = 10;
 
             int[] numbers = new int[numberOfColumns];
@@ -22,26 +18,24 @@ namespace _0023_Sorting_numbers
             for (int i = 0; i < numberOfColumns ; i++)
             {
                 numbers[i] = random.Next(minNumberRandomness, maxNumberRandomness);
-                Console.WriteLine($"{i + 1}-{ numbers[i]}");
+                Console.Write($"{ numbers[i]}, ");
             }
 
             for(int i = 0; i < numberOfColumns; i++)
             {
-                for(int j = 0; j < numberOfColumns - i; j++)
+                for(int j = 0; j < numberOfColumns - i - 1; j++)
                 {
                     if (numbers[j] > numbers[j + 1])
                     {
                         int temp = numbers[j];
-                        numbers[j] = numbers[j = 1];
+                        numbers[j] = numbers[j + 1];
                         numbers[j + 1] = temp;
                     }
                 }
             }
 
-            Console.WriteLine("Массив: " + string.Join(", ", numbers));
+            Console.WriteLine("\n\nЧисла массива отсортированы, от меньшего до большего:\n " + string.Join(", ", numbers));
             Console.ReadKey();
-
-
         }
     }
 }
