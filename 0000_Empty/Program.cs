a36bf65033dcb155a8fe62d145c5e0666b8f5beb
maksimovmaks <maksimum884@gmail.com>
2024-07-16 13:44:11 +0500

    0025_Shifting_array_values_001

---
+++ 0000_Empty/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0000_Empty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 0025_Shifting array values/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _0025_Shifting_array_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberOfColumns = 10;
+
+            int minNumberRandomness = -10;
+            int maxNumberRandomness = 10;
+
+            int[] numbers = new int[numberOfColumns];
+
+            int shift = 1;
+
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                numbers[i] = random.Next(minNumberRandomness, maxNumberRandomness);
+                Console.Write($"{numbers[i]}, ");
+            }
+
+            for (int i = 0; i < shift; i++)
+            {
+                int temp = numbers[0];
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    numbers[j] = numbers[j + 1];
+                }
+
+                numbers[numbers.Length - 1] = temp;
+            }
+
+            Console.WriteLine($"\n\nМассив после сдвига на {shift} позиций влево:");
+
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                Console.Write($"{numbers[i]}, ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
