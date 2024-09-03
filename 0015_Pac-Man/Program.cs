19d658b100094a311ed597614ef857892a290944
maksimovmaks <maksimum884@gmail.com>
2024-09-03 21:21:39 +0500

    0027_ReadInt_01

--- 0015_Pac-Man/Program.cs
+++ 0015_Pac-Man/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
-
+using System.Threading;//подключение многопоточности
+using System.Threading.Tasks;
 
 namespace _0015_Pac_Man
 {
@@ -8,18 +9,48 @@ namespace _0015_Pac_Man
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
             char[,] map = ReadMap("map.txt");
 
+            ConsoleKeyInfo pressedKey =  new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    pressedKey = Console.ReadKey();
+                }
+            });
+
+            int pacmanX = 1;
+            int pacmanY = 1;
+            int score = 0;
 
             while (true)
             {
+
                 Console.Clear();
+
+                Console.ForegroundColor = ConsoleColor.Blue;
                 DrawMap(map);
 
-                Console.SetCursorPosition(1, 1);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.SetCursorPosition(pacmanX, pacmanY);
                 Console.Write("@");
 
-                Thread.Sleep(1000)
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(39, 0);
+                Console.Write(pressedKey.KeyChar);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(29, 11);
+                Console.Write($"Score:{score}|");
+
+                //pressedKey = Console.ReadKey();//Thread.Sleep(1000);
+
+                HandleInput(pressedKey, ref pacmanX, ref pacmanY, map, ref score);
+
+                Thread.Sleep(1000);
+
             }
         }
 
@@ -40,7 +71,6 @@ namespace _0015_Pac_Man
         {
             for (int y = 0; y < map.GetLength(1); y++)
             {
-
                 for (int x = 0; x < map.GetLength(0); x++)
                 {
                     Console.Write(map[x, y]);
@@ -51,6 +81,66 @@ namespace _0015_Pac_Man
 
         }
 
+        private static void HandleInput(ConsoleKeyInfo pressedKey, ref int pacmanX, ref int pacmanY, char[,] map, ref int score)
+        {
+            int[] derection = GetDirection(pressedKey);
+
+            int nextPacmanPositionX = pacmanX + derection[0];
+            int nextPacmanPositionY = pacmanY + derection[1];
+
+            char nextCell = map[nextPacmanPositionX, nextPacmanPositionY];
+
+            if(nextCell == ' ' || nextCell == '.')
+            {
+                pacmanX = nextPacmanPositionX;
+                pacmanY = nextPacmanPositionY;
+
+                if(nextCell == '.')
+                {
+                    score++;
+                    map[nextPacmanPositionX, nextPacmanPositionY] = ' ';
+                }
+            }
+
+            /*
+            if (map[nextPacmanPositionX, nextPacmanPositionY] == ' ')
+            {
+                pacmanX = nextPacmanPositionX;
+                pacmanY = nextPacmanPositionY;
+            }
+            */
+
+            /*
+             if (pressedKey.Key == ConsoleKey.UpArrow)
+                pacmanY -= 1;
+            else if (pressedKey.Key == ConsoleKey.DownArrow)
+                pacmanY += 1;
+            else if (pressedKey.Key == ConsoleKey.LeftArrow)
+                pacmanX -= 1;
+            else if (pressedKey.Key == ConsoleKey.RightArrow)
+                pacmanX += 1;
+*/
+            /*
+            if(pacmanX < 0) pacmanX = 0;   
+            if(pacmanX > 429) { }
+            */
+        }
+
+        private static int[] GetDirection(ConsoleKeyInfo pressedKey) // направление берём от нажатой клавиши.
+        {
+            int[] direction = { 0, 0 };
+
+            if (pressedKey.Key == ConsoleKey.UpArrow)
+                direction[1] = -1;
+            else if (pressedKey.Key == ConsoleKey.DownArrow)
+                direction[1] = 1;
+            else if (pressedKey.Key == ConsoleKey.LeftArrow)
+                direction[0] = -1;
+            else if (pressedKey.Key == ConsoleKey.RightArrow)
+                direction[0] = 1;
+
+            return direction;
+        }
         private static int GetMaxLenthOfLine(string[] lines)
         {
             int maxLength = lines[0].Length;
---
+++ 0027_ReadInt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _0027_ReadInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = ReadNumberFromConsole();
+            Console.WriteLine($"Вы ввели: {number}");
+
+            Console.ReadKey();
+        }
+
+        static int ReadNumberFromConsole()
+        {
+            int number;
+            string input;
+            bool success = false;
+
+            do
+            {
+                Console.Write("Введите число:");
+                input = Console.ReadLine();
+
+                success = int.TryParse(input, out number);
+            }while(!success);
+
+            return number;
+        }
+    }
+}
