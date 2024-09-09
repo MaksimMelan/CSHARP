c9ca65a003877575046864ae640dec4d1cdf04c4
maksimovmaks <maksimum884@gmail.com>
2024-09-10 04:11:44 +0500

    0028_UIElement_03

--- 0028_UIElement/Program.cs
+++ 0028_UIElement/Program.cs
@@ -6,47 +6,59 @@ namespace _0028_UIElement
     {
         static void Main(string[] args)
         {
-            int health = 5, maxHealth = 10;
-            int mana = 7, maxMana = 10;
-            int satisfied = 8, maxSatisfied = 10;
-            int fatigueSun = 4, maxFatigueSun = 10;
-            int tiredness = 6, maxTiredness = 10;
+            int health = 5;
+            int maxHealth = 10;
 
-            DrawBar(health, maxHealth, ConsoleColor.Green, ConsoleColor.Red, 0, '#' );
+            int mana = 7;
+            int maxMana = 10;
+
+            int satisfied = 8;
+            int maxSatisfied = 10;
+
+            int fatigueSun = 4;
+            int maxFatigueSun = 10;
+
+            int tiredness = 6;
+            int maxTiredness = 10;
+
+            DrawBar(health, maxHealth, ConsoleColor.Green, ConsoleColor.Red, 0, '#');
             DrawBar(mana, maxMana, ConsoleColor.Blue, ConsoleColor.Red, 1, '@');
             DrawBar(satisfied, maxSatisfied, ConsoleColor.Yellow, ConsoleColor.Yellow, 2);
             DrawBar(fatigueSun, maxFatigueSun, ConsoleColor.White, ConsoleColor.White, 3);
             DrawBar(tiredness, maxTiredness, ConsoleColor.Red, ConsoleColor.Green, 4, '#');
 
             Console.ReadKey();
-
         }
-        static void DrawBar(int fillSizeScale, int maxSizeScale, ConsoleColor color, ConsoleColor textСolor, int position, char simbol = '_')
-        {
-            ConsoleColor defaultColor = Console.BackgroundColor;
 
+        static string FillBar(int stringLengh, char simbol)
+        {
             string bar = "";
 
-            for(int i = 0; i < fillSizeScale; i++)
-            {                
+            for (int i = 0; i < stringLengh; i++)
+            {
                 bar += simbol;
             }
+
+            return bar;
+        }
+
+        static void DrawBar(int fillSizeScale, int maxSizeScale, ConsoleColor color, ConsoleColor textСolor, int position, char simbolFill = '#', char simbolVoid = '_')
+        {
+            int emptySizeScale;
+
+            emptySizeScale = maxSizeScale - fillSizeScale;
+
+            ConsoleColor defaultColor = Console.BackgroundColor;
 
             Console.SetCursorPosition(0, position);
             Console.ForegroundColor = textСolor;
             Console.Write('[');
+
             Console.BackgroundColor = color;
-            Console.Write(bar);
+            Console.Write(FillBar(fillSizeScale, simbolFill));
+
             Console.BackgroundColor = defaultColor;
-
-            bar = "";
-
-            for( int i = fillSizeScale; i < maxSizeScale; i++)
-            {
-                bar += "_";
-            }
-
-            Console.Write(bar + ']');
+            Console.Write(FillBar(emptySizeScale , simbolVoid) + ']');
         }
     }
 }
