345a4254900d7705e30192e9a3d6ee92468c390f
maksimovmaks <maksimum884@gmail.com>
2026-04-02 15:29:11 +0500

    0033 L_Stack

---
+++ 0032_L_List/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0032_L_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(22);
+            numbers.Add(5);
+            numbers.Add(6);
+
+            numbers.AddRange(new int[] { 7, 8, 9, 10 });
+
+            numbers.Insert(1, 0);
+            numbers.Remove(6);
+            numbers.RemoveAt(3);
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+
+            }
+
+            Console.WriteLine(" Номер 22 находится в позиции " + (numbers.IndexOf(22) + 1));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 0032_L_Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0032_L_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> patients = new Queue<string>();
+            patients.Enqueue("Семёнов");
+            patients.Enqueue("Анисимов");
+            patients.Enqueue("Иванов");
+            patients.Enqueue("Ерёмин");
+            patients.Enqueue("Васильев");
+
+            Console.WriteLine("Удаляем первый элемент: " + patients.Dequeue());
+            Console.WriteLine("Новый первый элемент в очереди: " + patients.Peek());
+            Console.WriteLine("Все элементы в очереди:");
+            foreach (var patien in patients)
+            {
+                Console.WriteLine(patien);
+            }
+            Console.ReadKey();
+        }
+    }
+}
