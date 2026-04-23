a7d9686c9a22706713bf1a46a96c51d6aa04da40
maksimovmaks <maksimum884@gmail.com>
2026-04-24 02:52:54 +0500

    0034_L_Dictionary

---
+++ 0034_L_Dictionary/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0034_L_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> countriesCapitals = new Dictionary<string, string>();
+
+            //if (true)
+            //{
+            //    countriesCapitals.Add("Австралия", "Канберра"); 
+            //}
+
+            //if (countriesCapitals.ContainsKey("Австралия"))
+            //{
+            //    Console.WriteLine(countriesCapitals["Австралия"]);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Такой страны не существует.");
+            //}
+            {
+                countriesCapitals.Add("Австралия", "Канберра");
+                countriesCapitals.Add("Турция", "Анкара");
+                countriesCapitals.Add("Швейцария", "Берн");
+            }
+
+            //foreach( var item in countriesCapitals)
+            //{
+            //    Console.WriteLine($"Страна - {item.Key} столица {item.Value}");
+            //}
+
+            countriesCapitals.Remove("Турция");
+
+            foreach (var value in countriesCapitals.Values)
+            {
+                Console.WriteLine($"Страна -  столица {value}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
