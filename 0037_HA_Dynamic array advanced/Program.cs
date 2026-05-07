a7512d3550571c681c16f36aecea36ce7ec96c80
maksimovmaks <maksimum884@gmail.com>
2026-05-08 00:44:19 +0500

    0037_HA_Dynamic_array_advanced

---
+++ 0037_HA_Dynamic array advanced/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0037_HA_Dynamic_array_advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string AddCommand = "add";
+            const string SumCommand = "sum";
+            const string ExitCommand = "exit";
+
+            List<int> numbers = new List<int>();
+            int sum = 0;
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"{AddCommand} - добавить число.");
+                Console.WriteLine($"{SumCommand} - вывести сумму.");
+                Console.WriteLine($"{ExitCommand} - выход.");
+
+                OutputListNumbers(numbers);
+
+                Console.WriteLine("Введите команду: \n");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case AddCommand:
+                        AddNumber(numbers);
+                        break;
+
+                    case SumCommand:
+                        sum = SumList(numbers);
+                        Console.WriteLine($"Сумма чисел: {sum}");
+                        Console.ReadKey();
+                        break;
+
+                    case ExitCommand:
+                        Console.WriteLine("Нажмите любую клавишу и программа закроется");
+                        Console.ReadKey();
+                        isWork = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Вы ввели неизвестную команду");
+                        break;
+                }
+            }
+        }
+
+        private static void AddNumber(List<int> numbers)
+        {
+            string userInput;
+            int number;
+            do
+            {
+                Console.WriteLine("Введите число");
+                userInput = Console.ReadLine();
+
+            }
+            while (int.TryParse(userInput, out number) == false);
+
+            numbers.Add(number);
+        }
+
+        private static int SumList(List<int> numbers)
+        {
+            int sum = 0;
+
+            foreach (int i in numbers)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        private static void OutputListNumbers(List<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number}, ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 0037_HA_Dynamic array advanced/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("0037_HA_Dynamic array advanced")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("0037_HA_Dynamic array advanced")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5432c367-8ffb-45e1-9abe-e2537519d723")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
